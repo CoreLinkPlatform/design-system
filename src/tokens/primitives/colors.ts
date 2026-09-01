c52ce0f8dbce88b6844f173815e13729a8995d3b
Javid <javid.momeni@gmail.com>
2026-09-01 08:25:23 +0200

    feat(tokens): add primitive color tokens

---
+++ src/tokens/primitives/colors.ts
@@ -0,0 +1,13 @@
+export const primitiveColors = {
+  blue: {
+    500: '#0066FF',
+    600: '#0052CC',
+  },
+  cyan: {
+    500: '#00C2FF',
+  },
+  gray: {
+    50: '#F9FAFB',
+    900: '#111827',
+  },
+} as const;
