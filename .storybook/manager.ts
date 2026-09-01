1ff0ae6be7d7245511406ec5d79d3f8c1e14fb14
Javid <javid.momeni@gmail.com>
2026-09-01 08:19:13 +0200

    refactor: use core storybook theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,28 +1,8 @@
 import { addons } from '@storybook/manager-api';
-import { create } from '@storybook/theming';
-
-const theme = create({
-  base: 'light',
-
-  brandTitle: 'Core Platform Design System',
-  brandUrl: 'https://coreplatform.ir',
-  brandTarget: '_self',
-
-  fontBase: 'Noto Sans Arabic, Noto Sans, sans-serif',
-
-  colorPrimary: '#0066FF',
-  colorSecondary: '#00C2FF',
-
-  appBg: '#ffffff',
-  appContentBg: '#ffffff',
-  appBorderRadius: 16,
-
-  textColor: '#111827',
-  textInverseColor: '#ffffff',
-});
+import coreTheme from './core-theme';
 
 addons.setConfig({
-  theme,
+  theme: coreTheme,
   sidebar: {
     showRoots: true,
   },
