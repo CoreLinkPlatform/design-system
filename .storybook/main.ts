02f6b95825f90265304ebe2ba86d16c3683c7ae2
Javid <javid.momeni@gmail.com>
2026-09-01 08:00:11 +0200

    chore(storybook): prepare Core branded documentation setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../docs/**/*.mdx'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-themes',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
 
