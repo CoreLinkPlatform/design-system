889339ed67cbd7034ed805f823c33a0a15becb95
Javid <javid.momeni@gmail.com>
2026-09-02 16:52:43 +0200

    feat(experimental): expose tree-shakeable feedback exports

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,6 @@ export * from './components/EmptyState';
 export * from './tokens';
 export * from './icons';
 export * from './themes';
+export * from './experimental';
 
 import './styles/index.css';
